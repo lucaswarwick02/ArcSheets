a15428756b019b5fb121f3f42f33332916f97062
Lucas Warwick <30571400+lucaswarwick02@users.noreply.github.com>
2025-05-10 19:59:59 +0100

    Add "Add Entry" button to ArcSheet window

--- Editor/ArcSheets/ArcSheetWindow.cs
+++ Editor/ArcSheets/ArcSheetWindow.cs
@@ -1,11 +1,13 @@
 using System;
-using System.Numerics;
+using System.IO;
 using ArcSheets;
 using UnityEditor;
+using UnityEngine;
 
 public class ArcSheetWindow : EditorWindow
 {
     ArcSheet sheet;
+    Vector2 scroll;
 
     public static void Open(ArcSheet sheet)
     {
@@ -16,6 +18,51 @@ public class ArcSheetWindow : EditorWindow
 
     private void OnGUI()
     {
-        EditorGUILayout.HelpBox($"Type = {sheet.type}", MessageType.Info);
+        if (sheet == null || sheet.type == null)
+        {
+            EditorGUILayout.HelpBox("Sheet or sheet.type is not valid.", MessageType.Error);
+            return;
+        }
+
+        if (GUILayout.Button("Add New Entry"))
+        {
+            var entry = CreateInstance(sheet.type);
+            entry.name = sheet.type.Name;
+
+            string sheetPath = AssetDatabase.GetAssetPath(sheet);
+            string dir = Path.GetDirectoryName(sheetPath);
+            string folderName = Path.GetFileNameWithoutExtension(sheetPath);
+            string subDir = Path.Combine(dir, folderName);
+
+            if (!AssetDatabase.IsValidFolder(subDir))
+            {
+                _ = AssetDatabase.CreateFolder(dir, folderName);
+            }
+
+            string assetPath = AssetDatabase.GenerateUniqueAssetPath($"{subDir}/{sheet.type.Name}.asset");
+
+            AssetDatabase.CreateAsset(entry, assetPath);
+            AssetDatabase.SaveAssets();
+
+            sheet.entries.Add(entry);
+            EditorUtility.SetDirty(sheet);
+        }
+
+        scroll = EditorGUILayout.BeginScrollView(scroll);
+
+        foreach (var entry in sheet.entries)
+        {
+            if (entry == null)
+            {
+                continue;
+            }
+
+            EditorGUILayout.BeginVertical("box");
+            Editor editor = Editor.CreateEditor(entry);
+            editor.OnInspectorGUI();
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndScrollView();
     }
 }
